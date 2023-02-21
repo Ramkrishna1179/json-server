3474763b66b51424a04fea35eb206e848b75517a
Ramkrishna Yadav <ramkrashnayadav1998@gmail.com>
2023-02-21 17:53:53 +0530

    crud operation done we can make post delete update get requests

--- admin.js
+++ admin.js
@@ -39,9 +39,76 @@ async function Delete_post() {
   console.log(data);
 }
 
+async function Read_Data() {
+  let res = await fetch(`http://localhost:3000/posts/`);
+  let data = await res.json();
+  let tbdy = document.getElementById("tbdy");
+  data.map((data1) => {
+    let tr = document.createElement("tr");
+    let id_td = document.createElement("td");
+    id_td.innerHTML = data1.id;
 
-async function UpdatePost(){
+    let id_name = document.createElement("td");
+    id_name.innerHTML = data1.name;
 
+    let id_email = document.createElement("td");
+    id_email.innerHTML = data1.email;
 
+    let id_update = document.createElement("td");
+    id_update.innerHTML = "Update";
+    id_update.setAttribute("type","Button")
+    id_update.addEventListener("click", UpdatePost);
+    id_update.setAttribute("data-bs-toggle", "modal");
+    id_update.setAttribute("data-bs-target", "#staticBackdrop");
 
+    tr.append(id_td, id_name, id_email, id_update);
+    tbdy.append(tr);
+  });
+}
+
+async function UpdatePost(event) {
+  let TargetedID =
+    event.target.previousElementSibling.previousElementSibling
+      .previousElementSibling.innerHTML;
+  TargetedID = Number(TargetedID);
+  let res = await fetch(`http://localhost:3000/posts/${TargetedID}`);
+  let data = await res.json();
+  let id1 = document.getElementById("id1");
+  id1.value = data.id;
+
+  let name1 = document.getElementById("name1");
+  name1.value = data.name;
+
+  let email1 = document.getElementById("email1");
+  email1.value = data.email;
+  console.log(data);
+
+  //  let updateOnServer =5
+}
+
+async function PostUpdatetdData() {
+  let id = document.getElementById("id1").value;
+  let name = document.getElementById("name1").value;
+  let email = document.getElementById("email1").value;
+
+  let updatedData = {
+    id,
+    name,
+    email,
+  };
+
+  let postUpdatedData = await fetch(`http://localhost:3000/posts/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(updatedData),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  let res = await postUpdatedData.json();
+  console.log(res);
+
+  if (postUpdatedData.ok) {
+    alert("Data updated successfully!");
+  } else {
+    alert("Failed to update data");
+  }
 }
